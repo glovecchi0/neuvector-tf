73df2ed0f25321ad1ccf6a3521d47a5796ce1f3d
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2023-12-04 17:28:19 +0100

    Added ping on Harvester URL

--- tf-projects/neuvector-rke2/harvester-equinix/main.tf
+++ tf-projects/neuvector-rke2/harvester-equinix/main.tf
@@ -1,11 +1,16 @@
+locals {
+  private_ssh_key_path = fileexists("${path.cwd}/${var.prefix}-ssh_private_key.pem") ? "${path.cwd}/${var.prefix}-ssh_private_key.pem" : var.ssh_private_key_path
+  public_ssh_key_path  = fileexists("${path.cwd}/${var.prefix}-ssh_public_key.pem") ? "${path.cwd}/${var.prefix}-ssh_public_key.pem" : var.ssh_public_key_path
+}
+
 module "harvester-equinix" {
   source       = "../../../tf-modules/harvester/infrastructure/equinix"
   prefix       = var.prefix
   project_name = var.project_name
   metro        = var.metro
   #  create_ssh_key_pair  = var.create_ssh_key_pair
-  #  ssh_private_key_path = var.ssh_private_key_path
-  #  ssh_public_key_path  = var.ssh_public_key_path
+  #  ssh_private_key_path = local.private_ssh_key_path
+  #  ssh_public_key_path  = local.public_ssh_key_path
   #  ipxe_script          = var.ipxe_script
   #  vlan_count           = var.vlan_count
   #  instance_count       = var.instance_count
@@ -18,6 +23,27 @@ module "harvester-equinix" {
   #  rancher_access_key   = var.rancher_access_key
   #  rancher_secret_key   = var.rancher_secret_key
   #  rancher_insecure     = var.rancher_insecure
+}
+
+resource "null_resource" "wait-harvester-services-startup" {
+  depends_on = [module.harvester-equinix]
+  provisioner "local-exec" {
+    command     = <<-EOF
+      count=0
+      while [ "$${count}" -lt 5 ]; do
+        resp=$(curl -k -s -o /dev/null -w "%%{http_code}" $${HARVESTER_URL}ping)
+        echo "Waiting for $${HARVESTER_URL}ping - response: $${resp}"
+        if [ "$${resp}" = "200" ]; then
+          ((count++))
+        fi
+        sleep 2
+      done
+      EOF
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      HARVESTER_URL = module.harvester-equinix.harvester_url
+    }
+  }
 }
 
 #resource "null_resource" "first-setup" {
