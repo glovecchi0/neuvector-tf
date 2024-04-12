907394453b39dfcbe75ec36ea693aabb4fbed672
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-04-12 10:12:12 +0200

    Added timeout in draining script

--- tf-projects/neuvector-eks/drain-nodes.sh
+++ tf-projects/neuvector-eks/drain-nodes.sh
@@ -7,7 +7,7 @@ export KUBECONFIG="./$(cat ./terraform.tfvars | grep -v "#" | grep -i prefix | a
 for node in $(kubectl get nodes --no-headers | awk '{print $1}')
 do
   echo "Draining $node"
-  kubectl drain $node --ignore-daemonsets
+  kubectl drain $node --ignore-daemonsets --force --timeout=180s
 done
 
 #https://stackoverflow.com/questions/63128973/aws-on-terraform-error-deleting-resource-timeout-while-waiting-for-state-to-be
